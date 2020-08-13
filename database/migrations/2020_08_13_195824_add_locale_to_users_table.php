60a57fb16e4c0b89d4e21c4db84c7868035b8b3d
Kris <git@k118.de>
2020-08-13 20:16:10 +0200

    Completed migration

--- database/migrations/2020_08_13_195824_add_locale_to_users_table.php
+++ database/migrations/2020_08_13_195824_add_locale_to_users_table.php
@@ -26,7 +26,7 @@ class AddLocaleToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('locale');
         });
     }
 }
