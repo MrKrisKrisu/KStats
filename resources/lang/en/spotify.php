4e08ec918aad205f948f2a0d45e0fecd00ce841c
Kris <git@k118.de>
2020-08-13 20:52:38 +0200

    added english translations

---
+++ resources/lang/en/spotify.php
@@ -0,0 +1,58 @@
+<?php
+
+return [
+    'statistic' => 'Statistic',
+    'minutes' => [
+        'short' => 'min',
+        'singular' => 'Minute',
+        'plural' => 'Minutes',
+        'heared' => 'Minutes heared'
+    ],
+    'title' => [
+        'favourite_year' => 'Your favourite Year',
+        'favourite_bpm' => 'Your favourite Speed',
+        'count_tracks' => 'Listened tracks',
+        'avg_session_length' => 'do you hear on one piece',
+        'last_heared' => 'Last heared',
+        'top_tracks' => 'Your top tracks',
+        'top_artists' => 'Your top artists',
+        'heared_minutes_by_week' => 'Listened by week',
+        'heared_minutes_by_day' => 'Listened by day',
+        'heared_minutes_by_weekday' => 'Listened by weekday',
+        'heared_minutes_by_daytime' => 'Listened by daytime',
+        'lost_tracks' => 'Lost tracks',
+        'history' => 'History',
+        'heared_tracks' => 'Listened Tracks on :date'
+    ],
+    'total' => 'total',
+    'last_days' => 'last :days days',
+    'last_hours' => 'last :hours hours',
+    'time' => 'Time',
+    'time_suffix' => 'o\'clock',
+    'rank' => 'Rank',
+    'artist' => 'Artist',
+    'heared_minutes' => 'Listened minutes',
+    'find_tracks_again' => 'Rediscover old songs',
+    'create_playlist' => 'Create playlist',
+    'show_playlist' => 'Show playlist',
+    'device' => 'Device',
+    'track' => 'Track',
+    'place' => 'Rank',
+    'preview' => 'Preview',
+    'popular_track' => 'Currently popular track',
+    'lost_tracks' => [
+        'your_lost_tracks' => 'Your lost tracks',
+        'no_tracks' => 'You don\'t have any lost tracks. You can change the filter or come back in a few days.',
+        'max_tracks' => 'Maximum tracks in the playlist (max. :max)',
+        'daycount_lost_tracks' => 'Number of days until a song is considered "old"',
+        'minutes_until_liked' => 'Heard minutes after which a song is classified as "like"',
+        'description' => 'KStats creates a playlist with tracks that you haven\'t heard in a long time.'
+    ],
+    'no_track_found' => 'We cannot find any tracks.',
+    'play_music' => 'Play some music and come back then!',
+    'last_years' => 'Last years',
+    'last_months' => 'last 6 months',
+    'last_weeks' => 'last 4 weeks'
+
+
+];
