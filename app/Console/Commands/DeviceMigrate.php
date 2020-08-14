c2b65fc21ef863ee3ad58c85677100ecba9ac74c
Kris <git@k118.de>
2020-08-14 14:20:15 +0200

    Added argument to command
    #33

--- app/Console/Commands/DeviceMigrate.php
+++ app/Console/Commands/DeviceMigrate.php
@@ -13,7 +13,7 @@ class DeviceMigrate extends Command
      *
      * @var string
      */
-    protected $signature = 'kstats:migratedevice';
+    protected $signature = 'kstats:migratedevice {count=10}';
 
     /**
      * The console command description.
@@ -29,7 +29,7 @@ class DeviceMigrate extends Command
      */
     public function handle()
     {
-        $devices = SpotifyDeviceActivity::limit(10)->get();
+        $devices = SpotifyDeviceActivity::limit($this->argument("count"))->get();
         foreach ($devices as $device) {
             SpotifyPlayActivity::where('user_id', $device->device->user_id)
                 ->where('created_at', '>', $device->created_at->addMinutes(-2))
