8dec4312de0e995a116a3263b9773ba111ab50e5
Kris <git@k118.de>
2020-09-25 10:47:12 +0200

    Migrate factories

--- app/User.php
+++ app/User.php
@@ -3,40 +3,18 @@
 namespace App;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
 
-    use Notifiable;
+    use Notifiable, HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'username', 'email', 'password', 'last_login'
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    protected $fillable = ['username', 'email', 'password', 'last_login'];
+    protected $hidden = ['password', 'remember_token'];
+    protected $casts = ['email_verified_at' => 'datetime'];
 
     public function socialProfile()
     {
@@ -45,10 +23,8 @@ class User extends Authenticatable
 
     public function settings()
     {
-        return $this->hasMany('App\UserSettings', 'user_id', 'id');
+        return $this->hasMany(UserSettings::class, 'user_id', 'id');
     }
-
-    /* SPOTIFY */
 
     public function spotifyActivity()
     {
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,7 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
+        User::factory()->create(['username' => 'john.doe']);
+
         $this->call(SocialLoginProfileSeeder::class);
 
         //REWE eBon Analyzer Tables
