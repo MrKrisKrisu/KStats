ae009991b8912f79d5cbec35454798e788ca96cb
Kris <git@k118.de>
2020-11-09 17:53:13 +0100

    Moved CSS

--- resources/views/layout/cover.blade.php
+++ resources/views/layout/cover.blade.php
@@ -1,165 +1,45 @@
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
 
-    <title>KStats - Statistiken</title>
+        <title>KStats - Statistiken</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="/css/app.css" rel="stylesheet">
+        <link href="/css/app.css" rel="stylesheet">
+        <link href="/css/cover.css" rel="stylesheet">
+    </head>
 
-    <style>
-        /*
- * Globals
- */
+    <body class="text-center">
 
-        /* Links */
-        a,
-        a:focus,
-        a:hover {
-            color: #fff;
-        }
+        <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+            <header class="masthead mb-auto">
+                <div class="inner">
+                    <h3 class="masthead-brand">KStats</h3>
+                    <nav class="nav nav-masthead justify-content-center">
+                        <a class="nav-link active" href="{{ route('welcome') }}">{{__('general.menu.home')}}</a>
+                        @guest
+                            <a class="nav-link" href="{{ route('login') }}">{{__('auth.login')}}</a>
+                            <a class="nav-link" href="{{ route('register') }}">{{__('auth.register')}}</a>
+                        @else
+                            <a class="nav-link" href="{{ route('home') }}">{{__('general.menu.dashboard')}}</a>
+                        @endif
+                    </nav>
+                </div>
+            </header>
 
-        /* Custom default button */
-        .btn-secondary,
-        .btn-secondary:hover,
-        .btn-secondary:focus {
-            color: #333;
-            text-shadow: none; /* Prevent inheritance from `body` */
-            background-color: #fff;
-            border: .05rem solid #fff;
-        }
+            @yield('content')
 
-
-        /*
-         * Base structure
-         */
-
-        html,
-        body {
-            height: 100%;
-            background: url("{{url('/img/bg_cover.png')}}") no-repeat center center fixed;
-            -webkit-background-size: cover;
-            -moz-background-size: cover;
-            -o-background-size: cover;
-            background-size: cover;
-        }
-
-        body {
-            display: -ms-flexbox;
-            display: -webkit-box;
-            display: flex;
-            -ms-flex-pack: center;
-            -webkit-box-pack: center;
-            justify-content: center;
-            color: #fff;
-            text-shadow: 0 .05rem .1rem rgba(0, 0, 0, .5);
-            box-shadow: inset 0 0 5rem rgba(0, 0, 0, .5);
-        }
-
-        .cover-container {
-            max-width: 42em;
-        }
-
-
-        /*
-         * Header
-         */
-        .masthead {
-            margin-bottom: 2rem;
-        }
-
-        .masthead-brand {
-            margin-bottom: 0;
-        }
-
-        .nav-masthead .nav-link {
-            padding: .25rem 0;
-            font-weight: 700;
-            color: rgba(255, 255, 255, .5);
-            background-color: transparent;
-            border-bottom: .25rem solid transparent;
-        }
-
-        .nav-masthead .nav-link:hover,
-        .nav-masthead .nav-link:focus {
-            border-bottom-color: rgba(255, 255, 255, .25);
-        }
-
-        .nav-masthead .nav-link + .nav-link {
-            margin-left: 1rem;
-        }
-
-        .nav-masthead .active {
-            color: #fff;
-            border-bottom-color: #fff;
-        }
-
-        @media (min-width: 48em) {
-            .masthead-brand {
-                float: left;
-            }
-
-            .nav-masthead {
-                float: right;
-            }
-        }
-
-
-        /*
-         * Cover
-         */
-        .cover {
-            padding: 0 1.5rem;
-        }
-
-        .cover .btn-lg {
-            padding: .75rem 1.25rem;
-            font-weight: 700;
-        }
-
-
-        /*
-         * Footer
-         */
-        .mastfoot {
-            color: rgba(255, 255, 255, .5);
-        }
-    </style>
-</head>
-
-<body class="text-center">
-
-<div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
-    <header class="masthead mb-auto">
-        <div class="inner">
-            <h3 class="masthead-brand">KStats</h3>
-            <nav class="nav nav-masthead justify-content-center">
-                <a class="nav-link active" href="{{ route('welcome') }}">{{__('general.menu.home')}}</a>
-                @guest
-                    <a class="nav-link" href="{{ route('login') }}">{{__('auth.login')}}</a>
-                    <a class="nav-link" href="{{ route('register') }}">{{__('auth.register')}}</a>
-                @else
-                    <a class="nav-link" href="{{ route('home') }}">{{__('general.menu.dashboard')}}</a>
-                @endif
-
-            </nav>
+            <footer class="mastfoot mt-auto">
+                <div class="inner">
+                    <p>
+                        <a href="https://github.com/MrKrisKrisu/KStats"
+                           target="github">{{ __('general.show_sourcecode') }}</a> -
+                        <a href="/imprint/">{{__('general.menu.imprint')}}</a> -
+                        <a href="/privacy">{{ __('general.menu.privacy_policy') }}</a>
+                    </p>
+                </div>
+            </footer>
         </div>
-    </header>
-
-    @yield('content')
-
-    <footer class="mastfoot mt-auto">
-        <div class="inner">
-            <p>
-                <a href="https://github.com/MrKrisKrisu/KStats" target="github">{{ __('general.show_sourcecode') }}</a> -
-                <a href="/imprint/">{{__('general.menu.imprint')}}</a> -
-                <a href="/privacy">{{ __('general.menu.privacy_policy') }}</a>
-            </p>
-        </div>
-    </footer>
-</div>
-</body>
+    </body>
 </html>
