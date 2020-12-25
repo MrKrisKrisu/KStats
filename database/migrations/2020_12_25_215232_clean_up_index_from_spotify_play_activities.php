f464856f09946efcbacf64f4489731443e577d28
Kristian Stöckel <git@k118.de>
2020-12-25 21:53:12 +0100

    Create 2020_12_25_215232_clean_up_index_from_spotify_play_activities.php

---
+++ database/migrations/2020_12_25_215232_clean_up_index_from_spotify_play_activities.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CleanUpIndexFromSpotifyPlayActivities extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table('spotify_play_activities', function(Blueprint $table) {
+            $table->dropIndex(['user_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::table('spotify_play_activities', function(Blueprint $table) {
+            $table->index(['user_id']);
+        });
+    }
+}
