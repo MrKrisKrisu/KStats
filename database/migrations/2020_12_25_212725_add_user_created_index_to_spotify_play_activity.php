6d919dd9875bc29d7b5f36dff37a6bdfe4af6d98
Kristian Stöckel <git@k118.de>
2020-12-25 21:28:23 +0100

    Create 2020_12_25_212725_add_user_created_index_to_spotify_play_activity.php

---
+++ database/migrations/2020_12_25_212725_add_user_created_index_to_spotify_play_activity.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUserCreatedIndexToSpotifyPlayActivity extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table('spotify_play_activity', function(Blueprint $table) {
+            $table->index(['user_id', 'created_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::table('spotify_play_activity', function(Blueprint $table) {
+            $table->dropIndex(['user_id', 'created_at']);
+        });
+    }
+}
