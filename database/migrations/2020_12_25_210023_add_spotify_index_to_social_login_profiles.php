2d5426a67e6ff5912c325543a2cb3a2093d307b1
Kristian Stöckel <git@k118.de>
2020-12-25 21:01:12 +0100

    Create 2020_12_25_210023_add_spotify_index_to_social_login_profiles.php

---
+++ database/migrations/2020_12_25_210023_add_spotify_index_to_social_login_profiles.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddSpotifyIndexToSocialLoginProfiles extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table('social_login_profiles', function(Blueprint $table) {
+            $table->index(['spotify_accessToken', 'spotify_lastRefreshed']);
+            $table->index(['twitter_token']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::table('social_login_profiles', function(Blueprint $table) {
+            $table->dropIndex(['spotify_accessToken', 'spotify_lastRefreshed']);
+            $table->dropIndex(['twitter_token']);
+        });
+    }
+}
