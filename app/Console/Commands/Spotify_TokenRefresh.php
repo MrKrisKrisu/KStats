e5081ea19981fb221e0e348e721371e27d4fb67f
Kristian Stöckel <git@k118.de>
2020-12-30 13:58:42 +0100

    Update Spotify_TokenRefresh.php

--- app/Console/Commands/Spotify_TokenRefresh.php
+++ app/Console/Commands/Spotify_TokenRefresh.php
@@ -8,54 +8,31 @@ use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
 
-class Spotify_TokenRefresh extends Command
-{
+class Spotify_TokenRefresh extends Command {
 
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'spotify:tokenRefresh';
-
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
+    protected $signature   = 'spotify:tokenRefresh';
     protected $description = 'Get new AccessTokens to authorized Spotify Profiles';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
     }
 
-    /**
-     * Execute the console command
-     */
-    public function handle()
-    {
-        $slProfile = SocialLoginProfile::whereNotNull('spotify_accessToken')
-                                       ->where(function ($query) {
+    public function handle(): int {
+        $slProfile = SocialLoginProfile::with(['user'])
+                                       ->whereNotNull('spotify_accessToken')
+                                       ->where(function($query) {
                                            $query->where('spotify_lastRefreshed', '<', Carbon::now()->subMinutes(30));
                                            $query->where('spotify_lastRefreshed', '>', Carbon::now()->subDays(1));
                                        })
                                        ->orWhere('spotify_lastRefreshed', null)
                                        ->get();
 
-        foreach ($slProfile as $profile) {
+        foreach($slProfile as $profile) {
             try {
-                $user = $profile->user()->first();
-
                 $refreshedToken = SpotifyAPIController::getNewAccessToken($profile->spotify_refreshToken);
 
-                if (!$refreshedToken) {
-                    Log::error("[Spotify] [RefreshToken] Error while refreshing Token from User User " . $user->id . ". No Token is returned.");
+                if(!$refreshedToken) {
+                    Log::error("[Spotify] [RefreshToken] Error while refreshing Token from User User " . $profile->user->id . " / " . $profile->user->username . ". No Token is returned.");
                     continue;
                 }
 
@@ -64,7 +41,7 @@ class Spotify_TokenRefresh extends Command
                                      'spotify_lastRefreshed' => Carbon::now()
                                  ]);
 
-            } catch (\Exception $e) {
+            } catch(\Exception $e) {
                 report($e);
                 dump($e->getMessage());
             }
