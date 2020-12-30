e83a5a00fc596b3bb99f4a3147bb8c005f56732f
Kristian Stöckel <git@k118.de>
2020-12-30 03:12:06 +0100

    Improved MailVerification

--- app/Http/Controllers/UnauthorizedSettingsController.php
+++ app/Http/Controllers/UnauthorizedSettingsController.php
@@ -2,24 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use App\UserEmail;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
-class UnauthorizedSettingsController extends Controller
-{
-    public function verifyMail(Request $request, int $userId, string $verificationKey)
-    {
+class UnauthorizedSettingsController extends Controller {
+
+    public function verifyMail(Request $request, int $userId, string $verificationKey): RedirectResponse {
         $userEmail = UserEmail::where('unverified_user_id', $userId)->where('verification_key', $verificationKey)->first();
-        if ($userEmail == null) {
-            $request->session()->flash('alert-danger', __('settings.verify_mail.link_invalid'));
-            return redirect()->route('home');
+
+        if($userEmail == null) {
+            return redirect()->route('home')
+                             ->with('alert-danger', __('settings.verify_mail.link_invalid'));
         }
 
-        $userEmail->verified_user_id   = $userEmail->unverified_user_id;
-        $userEmail->unverified_user_id = null;
-        $userEmail->update();
-        $request->session()->flash('alert-success', __('settings.verify_mail.verified_successfully'));
+        $user = User::find($userId);
+        if($user->email == null)
+            $user->update([
+                              'email' => $userEmail->email
+                          ]);
 
-        return redirect()->route('home');
+        $userEmail->update([
+                               'verified_user_id'   => $userEmail->unverified_user_id,
+                               'unverified_user_id' => null,
+                               'verification_key'   => null
+                           ]);
+
+
+        return redirect()->route('home')
+                         ->with('alert-success', __('settings.verify_mail.verified_successfully'));
     }
 }
