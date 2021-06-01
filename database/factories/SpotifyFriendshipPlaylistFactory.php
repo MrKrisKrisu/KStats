8b9bd83dd9492f0700374f86b2a4339c24139cd6
Kristian Stöckel <git@k118.de>
2021-06-02 01:37:52 +0200

    Added factory

--- database/factories/SpotifyFriendshipPlaylistFactory.php
+++ database/factories/SpotifyFriendshipPlaylistFactory.php
@@ -4,25 +4,25 @@ namespace Database\Factories;
 
 use App\SpotifyFriendshipPlaylist;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\User;
+use JetBrains\PhpStorm\ArrayShape;
 
-class SpotifyFriendshipPlaylistFactory extends Factory
-{
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var string
-     */
+class SpotifyFriendshipPlaylistFactory extends Factory {
+
     protected $model = SpotifyFriendshipPlaylist::class;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
-    {
+    #[ArrayShape([
+        'user_id'        => "\Illuminate\Database\Eloquent\Factories\Factory",
+        'friend_id'      => "\Illuminate\Database\Eloquent\Factories\Factory",
+        'playlist_id'    => "string",
+        'last_refreshed' => "\DateTime"
+    ])]
+    public function definition(): array {
         return [
-            //
+            'user_id'        => User::factory(),
+            'friend_id'      => User::factory(),
+            'playlist_id'    => $this->faker->word,
+            'last_refreshed' => $this->faker->dateTime(),
         ];
     }
 }
