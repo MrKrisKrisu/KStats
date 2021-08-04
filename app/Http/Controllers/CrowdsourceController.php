ca9813a69363509036cc5f6221c501ba5f519470
Kristian Stöckel <git@k118.de>
2021-08-04 22:22:42 +0200

    Order

--- app/Http/Controllers/CrowdsourceController.php
+++ app/Http/Controllers/CrowdsourceController.php
@@ -39,6 +39,7 @@ class CrowdsourceController extends Controller {
                                             })
                                             ->groupBy('rewe_products.id')
                                             ->select(['rewe_products.*', DB::raw('MAX(rewe_bons.timestamp_bon) AS lastReceipt')])
+                                            ->orderByDesc(DB::raw('MAX(rewe_bons.timestamp_bon) AS lastReceipt'))
                                             ->limit(1)
                                             ->first();
 
