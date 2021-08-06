8966850c60c9ea1f3e3b87b01a2137b198d60d9a
Kristian Stöckel <git@k118.de>
2021-08-06 13:38:55 +0200

    Add api functions

--- app/Http/Controllers/Backend/Receipt/Grocy/ApiController.php
+++ app/Http/Controllers/Backend/Receipt/Grocy/ApiController.php
@@ -58,4 +58,46 @@ abstract class ApiController extends Controller {
         return json_decode($result->getBody()->getContents());
     }
 
+    public static function checkConnection(User $user): bool {
+        try {
+            $systemInfo = self::getSystemInfo($user);
+            if(!isset($systemInfo->grocy_version->Version)) {
+                return false;
+            }
+            return true;
+        } catch(NotConnectedException | GuzzleException) {
+            return false;
+        }
+    }
+
+    /**
+     * @param User   $user
+     * @param int    $amount
+     * @param float  $price
+     * @param string $barcode
+     *
+     * @return stdClass
+     * @throws GuzzleException
+     * @throws NotConnectedException
+     * @todo Waiting until https://github.com/grocy/grocy/pull/1565 is released...
+     */
+    public static function addToStockByBarcode(User $user, int $amount, float $price, string $barcode): stdClass {
+        $client = new Client();
+        $url    = strtr(':host/api/stock/products/by-barcode/:barcode/add', [
+            ':host'    => self::getGrocyHost($user),
+            ':barcode' => urlencode($barcode),
+        ]);
+        $result = $client->post($url, [
+            'headers' => [
+                'GROCY-API-KEY' => self::getApiKey($user),
+            ],
+            'json'    => [
+                'amount'           => $amount,
+                'transaction_type' => 'purchase',
+                'price'            => $price,
+            ],
+        ]);
+        return json_decode($result->getBody()->getContents());
+    }
+
 }
