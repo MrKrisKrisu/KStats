ae53ca7f99a9c3cdf048baf00d8ed52132068d26
Kris <git@k118.de>
2021-08-10 11:09:28 +0200

    🗃️ Improve database layout [Step 1] (#121)
    #75

--- app/Models/SpotifyPlayActivity.php
+++ app/Models/SpotifyPlayActivity.php
@@ -10,8 +10,12 @@ class SpotifyPlayActivity extends Model {
 
     use HasFactory;
 
-    protected $fillable = ['user_id', 'track_id', 'timestamp_start', 'progress_ms', 'context_id', 'device_id'];
+    protected $fillable = ['user_id', 'track_id', 'timestamp_start', 'duration', 'progress_ms', 'context_id', 'device_id'];
     protected $dates    = ['timestamp_start'];
+    protected $casts    = [
+        'duration'    => 'integer',
+        'progress_ms' => 'integer',
+    ];
 
     public function track(): BelongsTo {
         return $this->belongsTo(SpotifyTrack::class, 'track_id', 'id');
@@ -19,5 +23,9 @@ class SpotifyPlayActivity extends Model {
 
     public function device(): BelongsTo {
         return $this->belongsTo(SpotifyDevice::class, 'device_id', 'id');
+    }
+
+    public function context(): BelongsTo {
+        return $this->belongsTo(SpotifyContext::class, 'context_id', 'id');
     }
 }
