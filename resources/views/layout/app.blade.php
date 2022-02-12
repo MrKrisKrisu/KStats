a71627acde4e951bc9d82b1c033638a57afeb0bc
Kristian Stöckel <git@k118.de>
2022-02-13 00:34:15 +0100

    :art: Improve ui

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,9 +6,9 @@
 
         <title>@hasSection('title')@yield('title') - @endif{{__('KStats')}}</title>
 
-        <link rel="stylesheet" href="/css/app.css"/>
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}"/>
 
-        <script type="text/javascript" src="/js/app.js"></script>
+        <script type="text/javascript" src="{{ mix('js/app.js') }}"></script>
         <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
         <link rel="shortcut icon" type="image/x-icon" href="/favicon.svg">
