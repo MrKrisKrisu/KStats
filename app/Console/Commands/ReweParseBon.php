ec5f955f45edd375a8bddfe658cc9f718d901342
Kristian Stöckel <git@k118.de>
2022-02-12 22:59:54 +0100

    :art: Improve error logging

--- app/Console/Commands/ReweParseBon.php
+++ app/Console/Commands/ReweParseBon.php
@@ -11,45 +11,49 @@ use Spatie\PdfToText\Exceptions\PdfNotFound;
 use App\Http\Controllers\Backend\Receipt\ImportController;
 use Illuminate\Http\UploadedFile;
 use App\Http\Controllers\Backend\Receipt\Grocy\ReceiptController;
+use App\Exceptions\NotConnectedException;
+use Illuminate\Support\Facades\Log;
 
 class ReweParseBon extends Command {
 
     protected $signature = "rewe:parse {days=2}";
 
     public function handle(): int {
-
-        echo '* Fetch mails and attachments...' . PHP_EOL;
+        $this->info('* Fetch mails and attachments...');
 
         $files = ReweMailController::fetchMailAttachments($this->argument("days"));
 
-        echo strtr('* :count mails fetched!', [
-                ':count' => count($files),
-            ]) . PHP_EOL;
+        $this->info(strtr('* :count mails fetched!', [
+            ':count' => count($files),
+        ]));
 
         foreach($files as $bonAttachment) {
             try {
                 $userEmail = UserEmail::firstOrCreate(["email" => $bonAttachment->getEMail()]);
                 $user      = $userEmail->verifiedUser;
 
-                echo strtr('* Parse receipt from <:email>...', [':email' => $userEmail->email,]) . PHP_EOL;
+                $this->info(strtr('* Parse receipt from <:email>...', [':email' => $userEmail->email,]));
 
                 $filename     = $bonAttachment->getFilename();
                 $uploadedFile = new UploadedFile($filename, md5(time() . rand()) . '.pdf');
 
-                if($user != null) {
+                if($user !== null) {
                     $receipt = ImportController::parseReweReceipt($user, $uploadedFile);
-                    echo strtr('** Receipt successfully parsed. ID=:id', [':id' => $receipt->id]) . PHP_EOL;
+                    $this->info(strtr('** Receipt successfully parsed. ID=:id', [':id' => $receipt->id]));
                     if(isset($user->socialProfile->grocy_host) && $receipt->wasRecentlyCreated == 1) {
                         ReceiptController::addReceiptToStock($receipt);
                     }
                 } else {
-                    echo strtr('** There is no verified user for <:email>...', [':email' => $userEmail->email,]) . PHP_EOL;
+                    $this->info(strtr('** There is no verified user for <:email>...', [':email' => $userEmail->email,]));
                 }
-            } catch(ReceiptParseException $e) {
-                report($e);
-                dump("Error while parsing receipt. Is the format compatible?");
-            } catch(PdfNotFound $e) {
-                report($e);
+            } catch(ReceiptParseException|PdfNotFound $exception) {
+                $this->error('** Receipt could not be parsed!');
+                Log::error('** Receipt could not be parsed! ' . $bonAttachment->getEMail());
+                report($exception);
+            } catch(NotConnectedException $exception) {
+                $this->error('** Could not connect to Grocy!');
+                Log::error('** Could not connect to Grocy! ' . $bonAttachment->getEMail());
+                report($exception);
             }
         }
 
